76550e91cf8cc3f7d1dba76bd07873912f086609
a-neey <2371429@hansung.ac.kr>
2026-04-04 00:07:06 +0900

    🎨 프로젝트 초기 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,48 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import preferArrow from 'eslint-plugin-prefer-arrow';
+import prettierConfig from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      'prefer-arrow': preferArrow,
+      prettier,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          varsIgnorePattern: '^(is|set)[A-Z].*$',
+        },
+      ],
+      'prefer-arrow/prefer-arrow-functions': [
+        'error',
+        {
+          disallowPrototype: true,
+          singleReturnOnly: false,
+          classPropertiesAllowed: false,
+        },
+      ],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@components': '/src/components',
+        '@pages': '/src/pages',
+        '@hooks': '/src/hooks',
+        '@apis': '/src/apis',
+        '@routes': '/src/routes',
+        '@styles': '/src/styles',
+        '@utils': '/src/utils',
+        '@types': '/src/types',
+        '@mocks': '/src/mocks',
+        '@assets': '/src/assets',
+        '@store': '/src/store',
+        '@layouts': '/src/layouts',
+      },
+    },
+  };
+});
