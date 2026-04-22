91e72cccd70951ca5aaf21d72ed2a3460dbf5f86
a-neey <2371429@hansung.ac.kr>
2026-04-23 02:52:18 +0900

    ✨ 스플래시 화면 구현

--- src/components/splash/Splash.tsx
+++ src/components/splash/Splash.tsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import MainLogoIcon from '@assets/images/mainLogoIcon.svg';
+
 const Splash = () => {
+  const navigate = useNavigate();
+  const [fadeState, setFadeState] = useState<'fadeIn' | 'fadeOut'>('fadeIn');
+
+  useEffect(() => {
+    const fadeOutTimer = setTimeout(() => {
+      setFadeState('fadeOut');
+    }, 1200);
+
+    const navigateTimer = setTimeout(() => {
+      navigate('/home');
+    }, 1400);
+
+    return () => {
+      clearTimeout(fadeOutTimer);
+      clearTimeout(navigateTimer);
+    };
+  }, [navigate]);
+
   return (
-    <>
-      <p>스플래시</p>
-    </>
+    <div
+      className={`flex flex-col items-center justify-center min-h-screen bg-[#00408E] gap-4 transition-opacity duration-200 ${
+        fadeState === 'fadeIn' ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      <img src={MainLogoIcon} alt="로고" />
+      <p className="font-[KBLJump-EB-Extended] text-[#FFFFFF] text-[39px] leading-10.5 tracking-[2.4px]">
+        ZIGMA
+      </p>
+    </div>
   );
 };
 
