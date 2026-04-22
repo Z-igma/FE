ca8392fae875569e106875df7f89d7dcfd681ca1
a-neey <2371429@hansung.ac.kr>
2026-04-23 03:34:25 +0900

    ✨ 로그인 화면 뼈대 구현

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,8 +1,20 @@
+import MainLogoIcon from '@assets/images/mainLogoIcon.svg';
+import LoginButton from './components/LoginButton';
+
 const Login = () => {
   return (
-    <>
-      <p>로그인 페이지</p>
-    </>
+    <div className="h-full flex flex-col items-center justify-center gap-10">
+      <div className="flex flex-col items-center text-center gap-3">
+        <img src={MainLogoIcon} className="w-17 h-17" />
+        <p className="font-[KBLJump-EB-Extended] text-[#00408E] text-[1.25rem] leading-7 tracking-[0.1rem]">
+          ZIGMA
+        </p>
+      </div>
+      <div className="flex flex-col gap-3">
+        <LoginButton type="kakao" />
+        <LoginButton type="naver" />
+      </div>
+    </div>
   );
 };
 
---
+++ src/pages/login/components/LoginButton.tsx
@@ -0,0 +1,21 @@
+import KakaoIcon from '@assets/images/kakaoIcon.svg';
+import NaverIcon from '@assets/images/naverIcon.svg';
+
+const LoginButton = ({ type }: { type: 'kakao' | 'naver' }) => {
+  const isKakao = type === 'kakao';
+
+  return (
+    <button
+      className={`w-75 h-11 flex items-center justify-center gap-2 rounded-md ${isKakao ? 'bg-[#FEE500]' : 'bg-[#03C75A]'}`}
+    >
+      <img src={isKakao ? KakaoIcon : NaverIcon} alt={type} />
+      <p
+        className={`font-[Pretendard] font-semibold leading-6 ${isKakao ? 'text-[#111111]' : 'text-[#FFFFFF]'}`}
+      >
+        {isKakao ? '카카오 로그인' : '네이버 로그인'}
+      </p>
+    </button>
+  );
+};
+
+export default LoginButton;
