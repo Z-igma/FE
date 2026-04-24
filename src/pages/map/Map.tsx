d7682dc12da62ef57ff07232c922ea7299dfb8a1
a-neey <2371429@hansung.ac.kr>
2026-04-24 23:57:24 +0900

    ✨ 카카오맵 연동 및 기본 지도 렌더링

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from 'react';
+import { Map as KakaoMap } from 'react-kakao-maps-sdk';
+
 const Map = () => {
+  const [center, setCenter] = useState({ lat: 37.5823688, lng: 127.0111299 });
+
+  // 위치 허용 시 사용자 위치 기준으로 지도 표시
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setCenter({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
   return (
-    <>
-      <p>지도 페이지</p>
-    </>
+    <div className="w-full h-screen pb-24">
+      <KakaoMap center={center} style={{ width: '100%', height: '100%' }} />
+    </div>
   );
 };
 
