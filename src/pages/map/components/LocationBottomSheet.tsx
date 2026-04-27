bfeab7a1b01b887df842784207d1f5df2ae22585
a-neey <2371429@hansung.ac.kr>
2026-04-28 02:17:00 +0900

    ✨ 위치 선택 시 바텀 시트 추가

---
+++ src/pages/map/components/LocationBottomSheet.tsx
@@ -0,0 +1,53 @@
+import BottomSheet from '@/components/common/BottomSheet';
+import ProposalMemberIcon from '@/assets/images/map/proposalMemberIcon.svg';
+import PlusIcon from '@/assets/images/plusIcon.svg';
+
+interface LocationBottomSheetProps {
+  isOpen: boolean;
+  onClose: () => void;
+  placeName: string;
+  address: string;
+  proposedBy: string;
+}
+
+const LocationBottomSheet = ({
+  isOpen,
+  onClose,
+  placeName,
+  address,
+  proposedBy,
+}: LocationBottomSheetProps) => {
+  return (
+    <BottomSheet
+      isOpen={isOpen}
+      onClose={onClose}
+      className="flex flex-col px-4.5 pt-6 gap-15"
+      fullHeight="h-45"
+    >
+      <div>
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-0.5">
+            <p className="text-[#111111] font-Pretendard font-semibold text-[1.375rem] leading-7.7">
+              {placeName}
+            </p>
+            <div className="flex gap-0.5 items-center">
+              <img src={ProposalMemberIcon} />
+              <p className="text-[#888888] font-Pretendard font-medium text-[0.75rem] leading-4.2">
+                <span className="font-semibold">{proposedBy}</span> 님이 제안
+              </p>
+            </div>
+          </div>
+          <div className="w-9 h-9 p-1.5 bg-[#00408E] rounded-full">
+            <img src={PlusIcon} className="w-6 h-6" />
+          </div>
+        </div>
+        <p className="whitespace-pre-line text-[#111111] font-Pretendard font-regular text-[0.75rem] leading-4.5">
+          {`📍 ${address}`}
+          {/* {`📍 120m  서울 용산구 이태원동 123-45\n⏰ 11:00 - 22:00\n💸 2.5만원대`} */}
+        </p>
+      </div>
+    </BottomSheet>
+  );
+};
+
+export default LocationBottomSheet;
