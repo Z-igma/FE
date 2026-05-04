b08ade215bb4589cd16b02d92d3c443f010e49e9
a-neey <2371429@hansung.ac.kr>
2026-05-04 15:54:33 +0900

    ✨ 로그인 후 토큰 저장

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,13 +1,23 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface AuthState {
   isLoggedIn: boolean;
-  login: () => void;
+  accessToken: string | null;
+  login: (accessToken: string) => void;
   logout: () => void;
 }
 
-export const useAuthStore = create<AuthState>(set => ({
-  isLoggedIn: false,
-  login: () => set({ isLoggedIn: true }),
-  logout: () => set({ isLoggedIn: false }),
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    set => ({
+      isLoggedIn: false,
+      accessToken: null,
+      login: accessToken => set({ isLoggedIn: true, accessToken }),
+      logout: () => set({ isLoggedIn: false, accessToken: null }),
+    }),
+    {
+      name: 'auth',
+    },
+  ),
+);
