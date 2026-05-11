a4f7e492fcc62723df5979c2d30455fc73f20993
Jun279 <33.beautifulboy@gmail.com>
2026-05-11 18:06:35 +0900

    Fix: 브라우저 푸시 구독을 위한 VAPID키 변환

--- src/apis/apiHooks/usePushNotification.ts
+++ src/apis/apiHooks/usePushNotification.ts
@@ -1,15 +1,25 @@
 import { getVapidPublicKey, postPushSubscription } from "@/apis/notification/notification";
 
+const urlBase64ToUint8Array = (base64String: string) => {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = atob(base64);
+  return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)));
+};
+
 export const usePushNotification = () => {
   const subscribePushNotification = async () => {
     try {
       const vapidPublicKeyResponse = await getVapidPublicKey();
       const vapidPublicKey = vapidPublicKeyResponse.data.publicKey;
+      const convertedKey = urlBase64ToUint8Array(vapidPublicKey);
 
       const registration = await navigator.serviceWorker.ready; // 서비스 워커 가져오기(대기)
       const subscription = await registration.pushManager.subscribe({ // 푸시 구독 요청
         userVisibleOnly: true,
-        applicationServerKey: vapidPublicKey,
+        applicationServerKey: convertedKey,
       });
 
       await postPushSubscription({
