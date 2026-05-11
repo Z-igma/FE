cc00cf36288e3e705db7592998e87bfd93203110
Jun279 <33.beautifulboy@gmail.com>
2026-05-11 22:18:43 +0900

    Fix: push 알림 로그 추가

--- src/apis/apiHooks/usePushNotification.ts
+++ src/apis/apiHooks/usePushNotification.ts
@@ -11,9 +11,11 @@ const urlBase64ToUint8Array = (base64String: string) => {
 
 export const usePushNotification = () => {
   const subscribePushNotification = async () => {
+    console.log('1. 구독 시작');
     try {
       // 알림 권한 요청
       const permission = await Notification.requestPermission();
+      console.log('2. 권한:', permission);
       
       if (permission !== 'granted') {
         alert('알림 권한을 허용해야 알림을 받을 수 있어요!');
@@ -21,14 +23,17 @@ export const usePushNotification = () => {
       }
 
       const vapidPublicKeyResponse = await getVapidPublicKey();
+      console.log('3. VAPID 키:', vapidPublicKeyResponse);
       const vapidPublicKey = vapidPublicKeyResponse.data.publicKey;
       const convertedKey = urlBase64ToUint8Array(vapidPublicKey);
 
       const registration = await navigator.serviceWorker.ready; // 서비스 워커 가져오기(대기)
+      console.log('4. Service Worker:', registration);
       const subscription = await registration.pushManager.subscribe({ // 푸시 구독 요청
         userVisibleOnly: true,
         applicationServerKey: convertedKey,
       });
+      console.log('5. 구독 정보:', subscription);
 
       await postPushSubscription({
         endpoint: subscription.endpoint,
@@ -37,6 +42,7 @@ export const usePushNotification = () => {
           auth: btoa(String.fromCharCode(...new Uint8Array(subscription.getKey('auth')!))),
         }
       });
+      console.log('6. 구독 완료!');
     } catch (error) {
       console.error('푸시 구독 실패: ', error);
     }
